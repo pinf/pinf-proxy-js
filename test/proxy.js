1e350b1c194a4d1eed358b50f9da6b97c1ebaa9e
Christoph Dorn <christoph@christophdorn.com>
2013-10-21 23:00:34 -0700

    initial commit

---
+++ test/proxy.js
@@ -0,0 +1,14 @@
+
+const PATH = require("path");
+const FS = require("fs-extra");
+const ASSERT = require("assert");
+const PROXY = require("..");
+
+//const MODE = "test";
+const MODE = "write";
+
+const DEBUG = false;
+
+describe('proxy', function() {
+
+});
