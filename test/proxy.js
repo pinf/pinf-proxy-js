96c91ed405cc023e90d6b5b6f570513b7d705b8e
Christoph Dorn <christoph@christophdorn.com>
2013-10-24 13:09:22 -0700

    add proxy to request

--- test/proxy.js
+++ test/proxy.js
@@ -11,20 +11,44 @@ const MODE = "write";
 
 const DEBUG = false;
 
+const PROXY_PORT = 8082;
+const CONTENT_PORT = 8020;
+
 describe('proxy', function() {
 
 	it("can proxy requests", function(done) {
 		var contentServer = HTTP.createServer(function (req, res) {
+			ASSERT.equal(typeof req.headers["x-forwarded-for"], "string");
 			res.end("OK");
 		});
-		return contentServer.listen(8020, function() {
-			return PROXY.proxyPortTo(8082, "127.0.0.1", 8020, function(err, server) {
+		return contentServer.listen(CONTENT_PORT, function() {
+			return PROXY.proxyPortTo(PROXY_PORT, "127.0.0.1", CONTENT_PORT, function(err, proxy) {
 				if (err) return done(err);
-				return REQUEST("http://127.0.0.1:8082/", function(err, res, body) {
-					if (err) return next(err);
+				return REQUEST("http://127.0.0.1:" + PROXY_PORT + "/", function(err, res, body) {
+					if (err) return done(err);
 					ASSERT.equal(res.statusCode, 200);
 					ASSERT.equal(body, "OK");
-					return server.close(function() {
+					return proxy.close(function() {
+						return contentServer.close(done);
+					});
+				});
+			});
+		});
+	});
+
+	it("can add proxy to `request`", function(done) {
+		var contentServer = HTTP.createServer(function (req, res) {
+			ASSERT.equal(typeof req.headers["x-forwarded-for"], "string");
+			res.end("OK");
+		});
+		return contentServer.listen(CONTENT_PORT, function() {
+			return PROXY.proxyPortTo(PROXY_PORT, "127.0.0.1", CONTENT_PORT, function(err, proxy) {
+				if (err) return done(err);
+				return REQUEST(proxy.useForRequest("http://127.0.0.1:" + CONTENT_PORT + "/"), function(err, res, body) {
+					if (err) return done(err);
+					ASSERT.equal(res.statusCode, 200);
+					ASSERT.equal(body, "OK");
+					return proxy.close(function() {
 						return contentServer.close(done);
 					});
 				});
